0dd22173dad74283d0f532190f4ddd6bd3845afd
lisaiundralandi <alicjadudzinska5@gmail.com>
2022-04-19 19:43:09 +0200

    add password validation

--- src/main/java/io/github/lisaiundralandi/user/UserController.java
+++ src/main/java/io/github/lisaiundralandi/user/UserController.java
@@ -20,13 +20,37 @@ public class UserController {
 
     @PostMapping(path = "/user")
     public void createUser(@RequestBody @Valid UserRequest userRequest) {
-        if (!userRequest.getPassword()
-                .equals(userRequest.getPasswordConfirmations()) ||
-                userRepository.doesLoginExist(userRequest.getLogin())) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST);
+        String password = userRequest.getPassword();
+
+        if (!password.equals(userRequest.getPasswordConfirmations())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Passwords do not match");
+        } else if (userRepository.doesLoginExist(userRequest.getLogin())) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Login is already taken");
         }
+
+        boolean hasNoUpperCase = password.chars()
+                .noneMatch(ch -> Character.isUpperCase(ch));
+        boolean hasNoLowerCase = password.chars()
+                .noneMatch(ch -> Character.isLowerCase(ch));
+        boolean hasNoDigits = password.chars()
+                .noneMatch(ch -> Character.isDigit(ch));
+        boolean hasNoSpecialChars = password.chars()
+                .allMatch(ch -> Character.isLetterOrDigit(ch));
+
+        if (password.length() <= 8) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Password too short");
+        } else if (hasNoUpperCase) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Password has no upper case characters");
+        } else if (hasNoLowerCase) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Password has no lower case characters");
+        } else if (hasNoDigits) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Password has no digits");
+        } else if (hasNoSpecialChars) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Password has no special characters");
+        }
+
         userRepository.addUser(
-                new User(userRequest.getLogin(), userRequest.getPassword()));
+                new User(userRequest.getLogin(), password));
     }
 
     @PostMapping(path = "/login")
